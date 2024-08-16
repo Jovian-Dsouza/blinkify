e4f5d5784972c9be1b253fcd7f029cace7c7561a
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2024-08-16 12:36:27 +0000

    feat: add nextjs auth check in trpc

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,12 +1,25 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "@/server";
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { NextApiRequest } from "next";
+const secret = process.env.NEXTAUTH_SECRET;
 
 const handler = (req: Request) =>
   fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: appRouter,
-    createContext: () => ({}),
+    createContext: async () => {
+      const token = await getToken({
+        req: req as unknown as NextApiRequest,
+        secret,
+      });
+      // console.log("token", token);
+      return {
+        token,
+      };
+    },
   });
 
 export { handler as GET, handler as POST };
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,39 @@
-import { publicProcedure, router } from "./trpc";
+import { t, publicProcedure, router } from "./trpc";
 import { z } from "zod";
+import { TRPCError } from "@trpc/server";
+import { getSession } from "next-auth/react";
+
+const authMiddleware = t.middleware(async ({ ctx, next }) => {
+  //@ts-ignore
+  const token = ctx.token;
+  if (!token || !token.sub) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "You must be logged in to perform this action",
+    });
+  }
+  return next({
+    ctx: {
+      ...ctx,
+      token,
+    },
+  });
+});
 
 export const appRouter = router({
-	getTodos: publicProcedure.query(async () => {
-		return [10, 20, 30];
-	}),
-	addTodos: publicProcedure.input(z.string()).mutation(async (opts) => {
-		console.log(opts.input)
-		return true;
-	})
+  getTodos: publicProcedure.use(authMiddleware).query(async ({ ctx }) => {
+    const walletAddress = ctx.token.sub;
+    // console.log("walletAddress", walletAddress)
+    
+    return [10, 20, 30];
+  }),
+  addTodos: publicProcedure
+    .use(authMiddleware)
+    .input(z.string())
+    .mutation(async ({ input }) => {
+      console.log(input);
+      return true;
+    }),
 });
 
 export type AppRouter = typeof appRouter;
