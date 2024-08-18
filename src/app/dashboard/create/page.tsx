8d0b41ad5a8e94da89d2873bc394a97f87f69bdc
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2024-08-18 09:25:04 +0000

    feat: add create page

--- src/app/dashboard/create/page.tsx
+++ src/app/dashboard/create/page.tsx
@@ -1,16 +1,13 @@
 "use client";
-import { Typography } from "@mui/material";
 import PageContainer from "@/app/dashboard/components/container/PageContainer";
-import DashboardCard from "@/app/dashboard/components/shared/DashboardCard";
+import CreateAdvertisement from "@/components/CreateAdvertisement";
 
-const SamplePage = () => {
+const CreatePage = () => {
   return (
-    <PageContainer title="Sample Page" description="this is Sample page">
-      <DashboardCard title="Sample Page">
-        <Typography>This is a sample page</Typography>
-      </DashboardCard>
+    <PageContainer title="Create Page" description="Create a blink Ad">
+        <CreateAdvertisement />
     </PageContainer>
   );
 };
 
-export default SamplePage;
+export default CreatePage;
