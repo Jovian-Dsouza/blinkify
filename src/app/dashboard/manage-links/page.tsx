0d4790fb172bd461e61bc939c141cb1aa582701f
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2024-08-18 10:56:06 +0000

    feat: add manage links page

--- src/app/dashboard/manage-links/page.tsx
+++ src/app/dashboard/manage-links/page.tsx
@@ -2,15 +2,50 @@
 import { Typography } from "@mui/material";
 import PageContainer from "@/app/dashboard/components/container/PageContainer";
 import DashboardCard from "@/app/dashboard/components/shared/DashboardCard";
+import { trpc } from "@/app/_trpc/client";
+import { useEffect } from "react";
+import AdvertisementCard from "@/components/AdvertisementCard";
 
-const SamplePage = () => {
+const ManageLinks = () => {
+  const getAdvertisements = trpc.getAdvertisements.useQuery();
+
+  useEffect(() => {
+    if (getAdvertisements.status === "success") {
+      console.log(getAdvertisements.data);
+    } else if (getAdvertisements.status === "error") {
+      console.error(getAdvertisements.error);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [getAdvertisements.status, getAdvertisements.data]);
+
   return (
-    <PageContainer title="Sample Page" description="this is Sample page">
-      <DashboardCard title="Sample Page">
-        <Typography>This is a sample page</Typography>
+    <PageContainer
+      title="Manage Links"
+      description="This is the Manage Links page"
+    >
+      <DashboardCard title="Manage Links">
+        {getAdvertisements.isLoading ? (
+          <Typography>Loading...</Typography>
+        ) : getAdvertisements.isError ? (
+          <Typography>
+            Error loading advertisements: {getAdvertisements.error.message}
+          </Typography>
+        ) : (
+          <div className="flex flex-wrap -mx-4 p-4">
+            {/* Render your data here */}
+            {getAdvertisements.data?.map((ad) => (
+              <div
+                key={ad.id}
+                className="px-4 mb-6"
+              >
+                <AdvertisementCard {...ad} />
+              </div>
+            ))}
+          </div>
+        )}
       </DashboardCard>
     </PageContainer>
   );
 };
 
-export default SamplePage;
+export default ManageLinks;
