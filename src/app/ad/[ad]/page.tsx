e44f8b77dd62b9a1273fcbb754937f9dfc4242c9
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2024-08-19 03:03:22 +0000

    feat: add fallback page to dialect interstial

--- src/app/ad/[ad]/page.tsx
+++ src/app/ad/[ad]/page.tsx
@@ -1,10 +1,15 @@
-"use client";
-import React from "react";
+import { redirect } from "next/navigation";
 
-export default function AdPage() {
+export default function AdPage({ params }: { params: { ad: string } }) {
+  const { ad: id } = params;
+  const url = `https://www.blinkify.fun/ad/${id}`;
+  const cluster =
+    process.env.NEXT_PUBLIC_NETWORK === "devnet" ? "?cluster=devnet" : "";
+  redirect(`https://dial.to?action=${url}${cluster}`);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Ad page
+      Redirecting...
     </main>
   );
 }
