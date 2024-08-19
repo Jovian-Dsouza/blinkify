ac9bd22f6428df6810f29b4997ca0eab6345cb2b
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2024-08-19 04:03:52 +0000

    feat: update dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,9 +12,9 @@ const Dashboard = () => {
     <PageContainer title="Dashboard" description="this is Dashboard">
       <Box>
         <Grid container spacing={3}>
-          <Grid item xs={12} lg={8}>
+          {/* <Grid item xs={12} lg={8}>
             <SalesOverview />
-          </Grid>
+          </Grid> */}
           <Grid item xs={12} lg={4}>
             <Grid container spacing={3}>
               <Grid item xs={12}>
