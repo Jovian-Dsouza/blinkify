13f63ef1ba755b1731c508654fb0501411e6b5b0
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2024-08-20 19:00:10 +0000

    feat: update montly earning component

--- src/app/dashboard/components/dashboard/MonthlyEarnings.tsx
+++ src/app/dashboard/components/dashboard/MonthlyEarnings.tsx
@@ -2,13 +2,24 @@ import dynamic from "next/dynamic";
 const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 import { useTheme } from "@mui/material/styles";
 import { Stack, Typography, Avatar, Fab } from "@mui/material";
-import { IconArrowDownRight, IconCurrencyDollar } from "@tabler/icons-react";
+import {
+  IconArrowDownRight,
+  IconArrowUpLeft,
+  IconCurrencyDollar,
+} from "@tabler/icons-react";
 import { useSession } from "next-auth/react"; // Import useSession
 import DashboardCard from "@/app/dashboard/components/shared/DashboardCard";
+import { trpc } from "@/app/_trpc/client";
+import { useCluster } from "@/providers/cluster-provider";
 
 const MonthlyEarnings = () => {
   // Get session
   const { status } = useSession();
+
+  const { cluster } = useCluster();
+  const { data, isLoading, isError } = trpc.getMonthlyRevenue.useQuery({
+    network: cluster.networkName,
+  });
 
   // Masked values
   const maskedValue = "****";
@@ -19,6 +30,7 @@ const MonthlyEarnings = () => {
   const secondary = theme.palette.secondary.main;
   const secondarylight = "#f5fcff";
   const errorlight = "#fdede8";
+  const successlight = theme.palette.success.light;
 
   // chart
   const optionscolumnchart: any = {
@@ -78,16 +90,47 @@ const MonthlyEarnings = () => {
       }
     >
       <>
-        <Typography variant="h3" fontWeight="700" mt="-20px">
-          {status === "authenticated" ? "$0" : maskedValue}
-        </Typography>
-        <Stack direction="row" spacing={1} my={1} alignItems="center">
-          <Avatar sx={{ bgcolor: errorlight, width: 27, height: 27 }}>
-            <IconArrowDownRight width={20} color="#FA896B" />
-          </Avatar>
-          <Typography variant="subtitle2" fontWeight="600">
-            {status === "authenticated" ? "+0%" : maskedPercentage}
+        {isLoading ? (
+          <Typography variant="h3" fontWeight="700" mt="-20px">
+            Loading...
           </Typography>
+        ) : isError ? (
+          <Typography variant="h3" fontWeight="700" mt="-20px" color="error">
+            Error
+          </Typography>
+        ) : (
+          <Typography variant="h3" fontWeight="700" mt="-20px">
+            ${data.monthlyRevenue}
+          </Typography>
+        )}
+        <Stack direction="row" spacing={1} my={1} alignItems="center">
+          {isLoading ? (
+            <Typography variant="subtitle2" fontWeight="600">
+              Loading...
+            </Typography>
+          ) : isError ? (
+            <Typography variant="subtitle2" fontWeight="600" color="error">
+              Error
+            </Typography>
+          ) : (
+            <>
+              {data.percentageChange >= 0 ? (
+                <Avatar sx={{ bgcolor: successlight, width: 27, height: 27 }}>
+                  <IconArrowUpLeft width={20} color="#39B69A" />
+                </Avatar>
+              ) : (
+                <Avatar sx={{ bgcolor: errorlight, width: 27, height: 27 }}>
+                  <IconArrowDownRight width={20} color="#FA896B" />
+                </Avatar>
+              )}
+
+              <Typography variant="subtitle2" fontWeight="600">
+                {data.percentageChange
+                  ? `${data.percentageChange.toFixed(2)}%`
+                  : "+0%"}
+              </Typography>
+            </>
+          )}
           <Typography variant="subtitle2" color="textSecondary">
             last month
           </Typography>
